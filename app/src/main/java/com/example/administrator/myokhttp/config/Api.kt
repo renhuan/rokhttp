74347db612c6994c40a42117d0be7f4a710c633d
renhuan <13593854384>
2019-12-03 13:46:15 +0800

    迁移kotlin

---
+++ app/src/main/java/com/example/administrator/myokhttp/config/Api.kt
@@ -0,0 +1,14 @@
+
+object Api {
+
+//    fun sendRegisterVerify(phone: String, baseCall: RBaseOkHttpImp) {
+//        BaseOkHttp.newInstance()
+//                .setUrl(BuildConfig.BASE_URL + "/app/user/register/verify")
+//                .setCallBack(baseCall, Constants.GET_REGISTERVERIFY)
+//                .setParameter(hashMapOf(
+//                        "country" to "86",
+//                        "phone" to phone
+//                ))
+//                .postAsJson()
+//    }
+}
---
+++ app/src/main/java/com/example/administrator/myokhttp/config/BaseOkHttp.kt
@@ -0,0 +1,77 @@
+import android.text.TextUtils
+import com.example.okhttplib.config.RBaseOkHttp
+import com.example.okhttplib.config.RBaseOkHttpImp
+import com.lzy.okgo.model.HttpHeaders
+import com.lzy.okgo.model.Response
+import java.util.*
+
+/**
+ * http请求Base类
+ */
+
+class BaseOkHttp : RBaseOkHttp() {
+
+    override fun onRSuccess(response: Response<String>?, rBaseOkHttpImp: RBaseOkHttpImp?, requestCode: Int) {
+//        val topActivity = ActivityManager.getInstance().currentActivity() as BaseActivity
+//        response?.body()?.let {
+//            val jsonObject = JSONObject(it)
+//            val code = jsonObject.getInt("code")
+//            if (code == 200) {
+//                rBaseOkHttpImp?.onSuccess(it, requestCode)
+//            } else {
+//                val msg = jsonObject.getString("message")
+//                topActivity.toast(msg)
+//                rBaseOkHttpImp?.onError(it, requestCode)
+//            }
+//        }
+
+    }
+
+//    private var xPopup: LoadingPopupView? = null
+
+    override fun setHttpHead(httpHeaders: HttpHeaders): HttpHeaders {
+//        httpHeaders.put(Constants.HEAD, App.token)
+//        httpHeaders.put(Constants.IMEI, DeviceUtils.getAndroidID())
+//        httpHeaders.put(Constants.MAC, DeviceUtils.getMacAddress())
+        return httpHeaders
+    }
+
+    override fun setParameter(hashMap: HashMap<String, String?>): RBaseOkHttp {
+//        hashMap["ts"] = System.currentTimeMillis().toString()
+//        hashMap["sign"] = EncryptUtils.encryptMD5ToString(getSign(hashMap) + Constants.KEY)
+        return super.setParameter(hashMap)
+    }
+
+    override fun showLoading() {
+//        if (xPopup == null) {
+//            xPopup = XPopup.Builder(ActivityUtils.getTopActivity())
+//                    .dismissOnTouchOutside(false)
+//                    .hasShadowBg(false)
+//                    .asLoading()
+//                    .bindLayout(R.layout.loading_lottie)
+//        }
+//        xPopup?.show()
+    }
+
+    override fun hideLoading() {
+//        xPopup?.dismiss()
+    }
+
+    companion object {
+        fun newInstance(): BaseOkHttp {
+            return BaseOkHttp()
+        }
+
+        fun getSign(hashMap: HashMap<String, String?>): String {
+            val map = TreeMap<String, String?> { obj1, obj2 -> obj1.compareTo(obj2) }
+            map.putAll(hashMap)
+            val url = StringBuilder()
+            for ((key, value) in map) {
+                if (!TextUtils.isEmpty(value)) {
+                    url.append(key).append("=").append(value).append("&")
+                }
+            }
+            return url.substring(0, url.length - 1)
+        }
+    }
+}
