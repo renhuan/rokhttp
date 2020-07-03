b9511f327ae774d79ea4788973218da0aa7c0746
DESKTOP-1UM6EL8\hu <996513028@qq.com>
2020-07-03 16:18:40 +0800

    协程添加 onError方法，添加拦截器

--- okhttplib/src/main/java/com/renhuan/okhttplib/RApp.kt
+++ okhttplib/src/main/java/com/renhuan/okhttplib/RApp.kt
@@ -5,12 +5,19 @@ import android.app.Application
 import android.os.Bundle
 import androidx.multidex.MultiDex
 import com.blankj.utilcode.util.ActivityUtils
+import com.blankj.utilcode.util.PathUtils
 import com.blankj.utilcode.util.Utils
+import com.readystatesoftware.chuck.ChuckInterceptor
 import com.renhuan.okhttplib.utils.Renhuan
 import com.simple.spiderman.SpiderMan
 import com.tencent.mmkv.MMKV
 import me.jessyan.autosize.AutoSizeConfig
 import me.jessyan.autosize.unit.Subunits
+import okhttp3.OkHttpClient
+import rxhttp.RxHttpPlugins
+import rxhttp.wrapper.cahce.CacheMode
+import rxhttp.wrapper.param.RxHttp
+import java.io.File
 
 abstract class RApp : Application() {
 
@@ -24,6 +31,7 @@ abstract class RApp : Application() {
     override fun onCreate() {
         super.onCreate()
         setTheme(applicationInfo.theme)
+        initRequest()
         SpiderMan.init(this).setTheme(if (getSpiderTheme() == 0) R.style.SpiderManTheme_Dark else getSpiderTheme())
         MultiDex.install(this)
         registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
@@ -31,6 +39,17 @@ abstract class RApp : Application() {
         Renhuan.initialize(this)
         initAutoSize()
         init()
+    }
+
+    private fun initRequest() {
+        //设置日志拦截器
+        RxHttp.init(
+                OkHttpClient.Builder()
+                        .addInterceptor(ChuckInterceptor(this))
+                        .build()
+        )
+        //缓存最大10M 缓存时间永久
+        RxHttpPlugins.setCache(File(PathUtils.getExternalAppCachePath()), 10 * 1024 * 1024, CacheMode.ONLY_NETWORK, -1)
     }
 
     /**
