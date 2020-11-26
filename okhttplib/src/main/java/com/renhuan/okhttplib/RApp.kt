163d51ce0ecc41cd87bc97f790db361739bc712a
DESKTOP-1UM6EL8\hu <996513028@qq.com>
2020-11-26 17:46:39 +0800

    添加RBaseViewModel

--- okhttplib/src/main/java/com/renhuan/okhttplib/RApp.kt
+++ okhttplib/src/main/java/com/renhuan/okhttplib/RApp.kt
@@ -3,7 +3,18 @@ package com.renhuan.okhttplib
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import androidx.multidex.MultiDex
 import com.blankj.utilcode.util.ActivityUtils
+import com.blankj.utilcode.util.PathUtils
+import com.readystatesoftware.chuck.ChuckInterceptor
+import com.renhuan.okhttplib.utils.Renhuan
+import com.simple.spiderman.SpiderMan
+import com.tencent.mmkv.MMKV
+import okhttp3.OkHttpClient
+import rxhttp.RxHttpPlugins
+import rxhttp.wrapper.cahce.CacheMode
+import rxhttp.wrapper.param.RxHttp
+import java.io.File
 
 abstract class RApp : Application() {
 
@@ -13,6 +24,18 @@ abstract class RApp : Application() {
         super.onCreate()
         setTheme(applicationInfo.theme)
         registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
+        //设置日志拦截器
+        RxHttp.init(
+                OkHttpClient.Builder()
+                        .addInterceptor(ChuckInterceptor(this))
+                        .build()
+        )
+        //缓存最大10M 缓存时间永久
+        RxHttpPlugins.setCache(File(PathUtils.getExternalAppCachePath()), 10 * 1024 * 1024, CacheMode.ONLY_NETWORK, -1)
+        SpiderMan.init(this).setTheme(R.style.SpiderManTheme_Dark)
+        MultiDex.install(this)
+        MMKV.initialize(this)
+        Renhuan.initialize(this)
         init()
     }
 
